dc377fa096b0c1ff1b32bd2c60fdc43d80e5a92b
Fars Bein <fars.bein@ryerson.ca>
2020-02-04 09:58:49 -0500

    change look

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return (
         <header>
             <img src='https://i.pinimg.com/originals/50/50/34/5050349a19e38cc68efb69db6ef2a8c1.png' alt='funnyFrog' />
-            <p>Meme Generator</p>
+            <p>Meme Maker</p>
         </header>
     )
 }
