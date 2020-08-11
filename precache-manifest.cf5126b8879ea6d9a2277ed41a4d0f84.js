0c5c39344e3b4bc98f2c8c21e347646bfb259693
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-08-11 18:04:31 -0400

    Updates

---
+++ precache-manifest.cf5126b8879ea6d9a2277ed41a4d0f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62089d0a3abcea86ee6e1953e600c300",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d2775bf22c98c25e231",
+    "url": "/portfolio/static/css/main.b25dd29e.chunk.css"
+  },
+  {
+    "revision": "46d693170135a71c8ad9",
+    "url": "/portfolio/static/js/2.65e1d1ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.65e1d1ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d2775bf22c98c25e231",
+    "url": "/portfolio/static/js/main.60a1742b.chunk.js"
+  },
+  {
+    "revision": "2f9226199593b334a40c",
+    "url": "/portfolio/static/js/runtime-main.20063050.js"
+  }
+]);
